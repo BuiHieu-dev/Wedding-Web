65a10787a52cfbb1079ea045866dd92af9b05c52
DongChiMin <b22dccn300@gmail.com>
2026-07-29 13:46:36 +0700

    Update FloralCorner.jsx

--- src/components/FloralCorner.jsx
+++ src/components/FloralCorner.jsx
@@ -1,7 +1,9 @@
+const IMG_PATH = `${import.meta.env.BASE_URL}img`
+
 function FloralCorner({ position = 'tl', className = '' }) {
   // Corner2 = gốc góc trái trên, Corner1 = gốc góc phải dưới
   const useCorner2 = position === 'tl'
-  const imgSrc = useCorner2 ? '/img/Corner2.png' : '/img/Corner1.png'
+  const imgSrc = useCorner2 ? `${IMG_PATH}/Corner2.png` : `${IMG_PATH}/Corner1.png`
 
   // Corner1 is tall (1457x2046), Corner2 is wide (1466x1399)
   // Need objectPosition to anchor image to the correct corner
